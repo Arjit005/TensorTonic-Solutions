db9995a5248bd4f5a2bb8d1ef1fb0e0ca1ae1486
Arjit Katiyar <katiyarkarn295@gmail.com>
2026-04-19 03:37:38 +0530

    Update solution for metrics-f1-micro

--- metrics-f1-micro/metrics-f1-micro.py
+++ metrics-f1-micro/metrics-f1-micro.py
@@ -4,7 +4,10 @@ def f1_micro(y_true, y_pred) -> float:
     y_true = np.array(y_true)
     y_pred = np.array(y_pred)
 
-    correct = np.sum(y_true == y_pred)
+    correct = np.sum(y_true == y_pred)# number of correct predictions
+    # here FP==FN
+    # and for multiclass==> TP+FP= total sample
+    # so ,f1=correct prediction /total sample
     total = len(y_true)
-
+    
     return correct / total
