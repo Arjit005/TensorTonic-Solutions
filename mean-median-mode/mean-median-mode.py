0e61772a4b9dc57636722b30456a05b90142df01
Arjit Katiyar <katiyarkarn295@gmail.com>
2026-04-20 03:44:06 +0530

    Update solution for mean-median-mode

--- mean-median-mode/mean-median-mode.py
+++ mean-median-mode/mean-median-mode.py
@@ -9,7 +9,7 @@ def mean_median_mode(x):
     x=np.array(x,dtype='float')
     mean=np.mean(x)
     median=np.median(x)
-    freq=Counter(x)
+    freq=Counter(x)# gives output as dictionary 
     mode=max(freq,key=freq.get)
     return mean,median,mode
     
