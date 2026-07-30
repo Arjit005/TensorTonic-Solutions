60bd403657ef10607558ad36e56eaa857f8366c3
Arjit Katiyar <katiyarkarn295@gmail.com>
2026-07-30 23:51:52 +0530

    Solve tanh-activation

--- tanh-activation/tanh-activation.py
+++ tanh-activation/tanh-activation.py
@@ -5,8 +5,9 @@ def tanh(x):
     Implement Tanh activation function.
     """
     # Write code here
-    x=np.array(x)
-    nemo=np.exp(x)-np.exp(-x)
-    deno=np.exp(x)+np.exp(-x)
-    tanh=nemo/deno
-    return tanh
+    x=np.asarray(x,dtype=float)
+    exp_positive=np.exp(x)
+    exp_negative=np.exp(-x)
+    
+    result=(exp_positive-exp_negative)/(exp_positive+exp_negative)
+    return result
